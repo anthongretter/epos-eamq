1c5e893616e37e994e8958a13d9b3aee5c898cab
emilysk49 <emilytti@me.com>
2024-11-27 16:52:10 -0300

    arrumando as coisas que faltou

--- app/test_PMU/test_PMU_traits.h
+++ app/test_PMU/test_PMU_traits.h
@@ -110,6 +110,8 @@ template<> struct Traits<System>: public Traits<Build>
 
     static const unsigned int STACK_SIZE = Traits<Machine>::STACK_SIZE;
     static const unsigned int HEAP_SIZE = (Traits<Application>::MAX_THREADS + Traits<Build>::CPUS) * Traits<Application>::STACK_SIZE;
+
+    static const unsigned int RUN_TO_HALT = true;
 };
 
 template<> struct Traits<Thread>: public Traits<Build>
